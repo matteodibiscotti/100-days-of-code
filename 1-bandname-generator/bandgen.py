66bee4ec95985b59731e7dc5c07082ae472e3169
Matthew Lowman <matthewlowman@Matthews-MacBook-Air.local>
2023-01-23 22:59:10 +0900

    Initial commit

---
+++ 1-bandname-generator/bandgen.py
@@ -0,0 +1,8 @@
+def main():
+    print ("Welcome to BAND GENERATOR!!")
+    city = input("What city were you born in?\n")
+    pet = input("What is your pet's name?\n")
+    print (' '.join([city, pet]))
+
+if __name__ == "__main__":
+    main()
