ff9cc277fc89f2a323b80d694b74970f3329e510
Matthew Lowman <matthew.lowman9@gmail.com>
2023-02-02 23:13:07 +0900

    Day 8 task complete

--- 8-caesar-cipher/encrypt-decrypt.py
+++ 8-caesar-cipher/encrypt-decrypt.py
@@ -1,42 +1,35 @@
-import ascii_art
+import ascii_art, string
+
+alphabet = string.ascii_lowercase
 
 def encrypt(string, shift):
-    # stuff
+    encrypted_string = ""
+    for i in string:
+        index = alphabet.find(i)
+        encrypted_char = alphabet[(index + shift) % 26]
+        encrypted_string += encrypted_char
+    return encrypted_string
 
 def decrypt(string, shift):
-    # stuff
+    decrypted_string = ""
+    for i in string:
+        index = alphabet.find(i)
+        decrypted_char = alphabet[(index - shift) % 26]
+        decrypted_string += decrypted_char
+    return decrypted_string
 
 def main():
     print(ascii_art.logo)
     print("Welcome to the Caesar Cipher encrypter/decrypter")
-    action = int(input("Would you like to encrypt or decrypt your string? (select '1' for encrypt or '2' for decrypt\n\n"))
+    action = int(input("Would you like to encrypt or decrypt your string? (select '1' for encrypt or '2' for decrypt\n"))
     if action == 1:
-        string = input("Please enter the string you would like to encrypt:\n\n")
-        shift = input("Enter the shift/key")
-        encrypt(string, shift)
+        string = input("Please enter the string you would like to encrypt:\n").lower()
+        shift = int(input("Enter the shift/key\n"))
+        print(encrypt(string, shift))
     else:
-        string = input("Please enter the string you would like to decrypt:\n\n")
-        shift = input("Enter the shift/key")
-        decrypt(string, shift)
+        string = input("Please enter the string you would like to decrypt:\n").lower()
+        shift = int(input("Enter the shift/key\n"))
+        print(decrypt(string, shift))
     
 if __name__ == "__main__":
     main()
-
-
-'''
-show the ascii art
-greeting to the tool
-ask for the string to encrypt
-ask for the number of shifts
-loop through the string
-match the letter in the string to the letter in the alphabet and return its position
-add the number of shifts to the position of the matched letter and return the letter at that position
-append that letter to an empty string
-print the output
-
-for decoding just do the opposite
-
-make an encode function and a decode function
-ask the user to choose
-
-'''
