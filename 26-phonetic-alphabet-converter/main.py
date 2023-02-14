b330ef35d3b7152b3e12c67c16ba288fc82963f2
Matthew Lowman <matthew.lowman9@gmail.com>
2023-02-14 21:23:48 +0900

    Added exception handling

--- 26-phonetic-alphabet-converter/main.py
+++ 26-phonetic-alphabet-converter/main.py
@@ -4,15 +4,17 @@ def main():
     data = pd.read_csv("nato_phonetic_alphabet.csv")
     alphabet = {row["letter"]:row["code"] for (index, row) in data.iterrows()} # Dictionary comprehension
 
-    word = input("Enter the word you want to translate into NATO phonetic:\n").upper()
-    # translated_word = []
-    # for letter in word:
-    #     for (key, value) in alphabet.items():
-    #         if key == letter:
-    #             translated_word.append(value)
-    translated_word = [alphabet[letter] for letter in word] # List comprehension
-
-    print(translated_word)
+    def translate():
+        word = input("Enter the word you want to translate into NATO phonetic:\n").upper()
+        try:
+            translated_word = [alphabet[letter] for letter in word] # List comprehension
+        except KeyError:
+            print("That is not a valid entry containing only letters of the alphabet")
+            translated_word = translate()
+        else:
+            print(translated_word)
+    
+    translate()
 
 if __name__ == "__main__":
     main()
