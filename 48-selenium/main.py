37b5c7d2c7f19afaa9331be0b64ce2cebc7b635e
Matthew Lowman <mlowmanprofessional@gmail.com>
2023-03-01 17:29:20 +0900

    Get price of item

--- 48-selenium/main.py
+++ 48-selenium/main.py
@@ -1,4 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
-chrome_driver_path = '/Users/matthewlowman/Coding/development/chromedriver'
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
+service = Service('/Users/matthewlowman/Coding/development/chromedriver')
+driver = webdriver.Chrome(service=service)
+
+driver.get('https://www.amazon.co.jp/-/en/KJ-65X80WK-Bravia-Compatible-Google-Recommended/dp/B0B3TFBQMW/ref=sr_1_8?crid=2L3RLT54U7E4&keywords=sony+tvx85k&qid=1677658067&sprefix=sony+tv+x85k%2Caps%2C171&sr=8-8')
+
+price = driver.find_element(By.CLASS_NAME, "a-price-whole")
+
+print(f'{price.text}円')
+
+driver.quit()
