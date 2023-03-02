78f7e6d31635a918844f90480ff7bfbb1f2a7f8a
Matthew Lowman <mlowmanprofessional@gmail.com>
2023-03-02 18:59:49 +0900

    Regex to extract model name

--- 47-amazon-price-checker/main.py
+++ 47-amazon-price-checker/main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import lxml
+import re
 
 URL = 'https://www.amazon.co.jp/-/en/KJ-65X80WK-Bravia-Compatible-Google-Recommended/dp/B0B3TFBQMW/ref=sr_1_8?crid=2L3RLT54U7E4&keywords=sony+tvx85k&qid=1677658067&sprefix=sony+tv+x85k%2Caps%2C171&sr=8-8'
 
@@ -16,10 +17,9 @@ def main():
     soup = BeautifulSoup(website_html, 'lxml')
 
     price = soup.find(class_="a-price-whole")
-    all_anchor_tags = soup.find_all(class_='a-size-base prodDetAttrValue')
-    model = "".join((all_anchor_tags[7].getText()).split())
-    print(model)
-    print(len(model))
+    product_details = soup.find_all(class_='a-size-base prodDetAttrValue')
+    model = re.findall("[A-Z]{2}-[0-9A-Z]{7}", product_details[7].getText())
+    print(model[0])
     print(f'{price.text}円')
 
 
