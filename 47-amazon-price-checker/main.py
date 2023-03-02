20e36003304d60d9db7a47b418eaf840ef56131b
Matthew Lowman <mlowmanprofessional@gmail.com>
2023-03-02 18:04:46 +0900

    Retrieve price from page

--- 47-amazon-price-checker/main.py
+++ 47-amazon-price-checker/main.py
@@ -5,15 +5,17 @@ import lxml
 URL = 'https://www.amazon.co.jp/-/en/KJ-65X80WK-Bravia-Compatible-Google-Recommended/dp/B0B3TFBQMW/ref=sr_1_8?crid=2L3RLT54U7E4&keywords=sony+tvx85k&qid=1677658067&sprefix=sony+tv+x85k%2Caps%2C171&sr=8-8'
 
 def main():
-    response = requests.get(URL)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8'
+    }
+
+    response = requests.get(url=URL, headers=headers)
     website_html = response.text
-    soup = BeautifulSoup(website_html, 'html.parser')
+    soup = BeautifulSoup(website_html, 'lxml')
 
-    movie_list = soup.find_all(name="h3", class_="title")
-
-    for title in reversed(movie_list):
-        with open('top100movies.txt', 'a', encoding="utf-8") as file:
-            file.write(title.getText() + '\n')
+    price = soup.find(class_="a-price-whole")
+    print(f'{price.text}円')
 
 if __name__ == "__main__":
     main()
