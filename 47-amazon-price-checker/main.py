b52e587219cee1f735539284c0180113a6f4a308
Matthew Lowman <mlowmanprofessional@gmail.com>
2023-03-02 18:46:28 +0900

    Retrieve model name

--- 47-amazon-price-checker/main.py
+++ 47-amazon-price-checker/main.py
@@ -5,6 +5,7 @@ import lxml
 URL = 'https://www.amazon.co.jp/-/en/KJ-65X80WK-Bravia-Compatible-Google-Recommended/dp/B0B3TFBQMW/ref=sr_1_8?crid=2L3RLT54U7E4&keywords=sony+tvx85k&qid=1677658067&sprefix=sony+tv+x85k%2Caps%2C171&sr=8-8'
 
 def main():
+
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
         'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8'
@@ -15,7 +16,12 @@ def main():
     soup = BeautifulSoup(website_html, 'lxml')
 
     price = soup.find(class_="a-price-whole")
+    all_anchor_tags = soup.find_all(class_='a-size-base prodDetAttrValue')
+    model = "".join((all_anchor_tags[7].getText()).split())
+    print(model)
+    print(len(model))
     print(f'{price.text}円')
+
 
 if __name__ == "__main__":
     main()
