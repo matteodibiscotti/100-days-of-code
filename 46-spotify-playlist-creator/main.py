c34997ad4b474968dd789dd76efc94e6635f70fd
Matthew Lowman <mlowmanprofessional@gmail.com>
2023-03-05 23:20:47 +0900

    Render html an pass to beautiful soup

--- 46-spotify-playlist-creator/main.py
+++ 46-spotify-playlist-creator/main.py
@@ -1,8 +1,24 @@
 from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 
 def main():
-    date = input('Which year do you want to travel to?  Enter the date in the format YYYY-MM-DD:')
-    
+    # date = input('Which year do you want to travel to?  Enter the date in the format YYYY-MM-DD:')
+    # url = f'https://www.billboard.com/charts/hot-100/{date}/'
+    url = f'https://www.billboard.com/charts/hot-100/1990-01-06/'
+
+    session = HTMLSession()
+    r = session.get(url)
+    r.html.render(timeout=20, sleep=3)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    song_html = soup.select('li ul li h3')
+    song_list = [song.getText().strip('\t\n') for song in song_html]
+    # print(song_list)
+
+    for i in range(5):
+        print(song_list[i])
+    #     # print(artist_list[i].getText())
+    #     print('\n')
 
 if __name__ == "__main__":
     main()
