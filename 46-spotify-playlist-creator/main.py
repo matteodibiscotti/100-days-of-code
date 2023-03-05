82f8b9285fcd4311d13aa7f9ebd1007a09990887
Matthew Lowman <mlowmanprofessional@gmail.com>
2023-03-05 15:44:22 +0900

    Initial commit for day 46 task

---
+++ 46-spotify-playlist-creator/main.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+
+def main():
+    date = input('Which year do you want to travel to?  Enter the date in the format YYYY-MM-DD:')
+    
+
+if __name__ == "__main__":
+    main()
