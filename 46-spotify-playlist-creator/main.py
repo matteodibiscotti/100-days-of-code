c307a277da83a2be76402595bf4c7aa101f4fd4f
Matthew Lowman <mlowmanprofessional@gmail.com>
2023-03-06 10:39:41 +0900

    Retrieved list of artist names

--- 46-spotify-playlist-creator/main.py
+++ 46-spotify-playlist-creator/main.py
@@ -8,17 +8,14 @@ def main():
 
     session = HTMLSession()
     r = session.get(url)
-    r.html.render(timeout=20, sleep=3)
+    r.html.render(timeout=20, sleep=3) # All page data wasn't loading so this renders the entire page
     soup = BeautifulSoup(r.text, 'html.parser')
 
     song_html = soup.select('li ul li h3')
     song_list = [song.getText().strip('\t\n') for song in song_html]
-    # print(song_list)
 
-    for i in range(5):
-        print(song_list[i])
-    #     # print(artist_list[i].getText())
-    #     print('\n')
+    artist_html = soup.select('li ul li span')
+    artist_list = [artist.getText().strip('\t\n') for artist in artist_html[0::7]]
 
 if __name__ == "__main__":
     main()
