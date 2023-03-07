ae84394089cf9807f1d57e60e9bc68dfc378b6b5
Matthew Lowman <mlowmanprofessional@gmail.com>
2023-03-07 20:51:07 +0900

    Return song URIs

--- 46-spotify-playlist-creator/main.py
+++ 46-spotify-playlist-creator/main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
+import requests
 import spotipy
 import os
 
@@ -9,6 +10,7 @@ redirect_uri = os.getenv('SPOTIPY_REDIRECT_URI')
 
 def main():
     # date = input('Which year do you want to travel to?  Enter the date in the format YYYY-MM-DD:')
+    year = date[:4]
     # url = f'https://www.billboard.com/charts/hot-100/{date}/'
     url = f'https://www.billboard.com/charts/hot-100/1990-01-06/'
 
@@ -24,6 +26,7 @@ def main():
     artist_list = [artist.getText().strip('\t\n') for artist in artist_html[0::7]]
 
     # ================SPOTIFY===================
+    # ================AUTH======================
 
     # Authenticate with spotify (creates an auth object that can be passed to the API client)
     auth = spotipy.oauth2.SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
@@ -31,7 +34,19 @@ def main():
     # Creates a spotify API client
     client = spotipy.client.Spotify(oauth_manager=auth)
 
-    print(client.current_user())
+    # ================SONG-URI======================
+    song_uri_list = []
+
+    # for i, j in zip(song_list, artist_list):
+    #     track, artist = i, j
+    #     query = f'track:{track} year:{year}'
+    #     response = (client.search(q=q, type='track', market='JP'))
+    #     # need to loop through the results to find the song where the song name and artist match their respective fields
+    #     song_uri_list.append(result) 
+
+
+
+    
 
 if __name__ == "__main__":
     main()
