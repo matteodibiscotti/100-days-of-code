909f0764b75e8cd81564d6bd68e57e1ba06d151b
Matthew Lowman <mlowmanprofessional@gmail.com>
2023-03-07 16:40:06 +0900

    Spotify auth

--- 46-spotify-playlist-creator/main.py
+++ 46-spotify-playlist-creator/main.py
@@ -1,5 +1,11 @@
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
+import spotipy
+import os
+
+client_id = os.getenv('SPOTIPY_CLIENT_ID')
+client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
+redirect_uri = os.getenv('SPOTIPY_REDIRECT_URI')
 
 def main():
     # date = input('Which year do you want to travel to?  Enter the date in the format YYYY-MM-DD:')
@@ -16,6 +22,16 @@ def main():
 
     artist_html = soup.select('li ul li span')
     artist_list = [artist.getText().strip('\t\n') for artist in artist_html[0::7]]
+
+    # ================SPOTIFY===================
+
+    # Authenticate with spotify (creates an auth object that can be passed to the API client)
+    auth = spotipy.oauth2.SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+
+    # Creates a spotify API client
+    client = spotipy.client.Spotify(oauth_manager=auth)
+
+    print(client.current_user())
 
 if __name__ == "__main__":
     main()
